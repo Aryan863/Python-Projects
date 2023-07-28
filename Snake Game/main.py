35e464ca1933544df3f46e6c71700b388ed6ff36
Aryan <aryanvkashyap03@gmail.com>
2023-07-28 09:35:44 +0530

    Snake game version 1.0

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -1,7 +1,11 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
+from score_board import ScoreBoard
 
+X_END = 282
+Y_END = 282
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,6 +14,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()                         # Event listeners not needed in loop
 screen.onkey(key="Up", fun=snake.up)    #higher order function. hence while passing function name, () paranthesis is not needed
@@ -22,10 +28,25 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
     
+# Detect collision with food
+    if snake.head.distance(food) < 15:
+        snake.extend()
+        food.refresh()
+        scoreboard.increase_score()
 
+# Detect collision with walls
+    if snake.head.xcor() >X_END or snake.head.xcor() < -X_END or snake.head.ycor() >Y_END or snake.head.ycor() < -Y_END:
+        game_is_on = False
+        scoreboard.game_over()
+
+# detect collision with body
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    
     
 
 
